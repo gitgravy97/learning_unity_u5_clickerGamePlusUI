073dd1de1d3ec01af245a399fb889797d187b0e8
Gravy Gatsby <mcshaned@umich.edu>
2025-09-29 23:15:53 -0500

    random item spawn with thrower and torque physics

--- Assets/Course Library/Scripts/Target.cs
+++ Assets/Course Library/Scripts/Target.cs
@@ -2,15 +2,45 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+    private Rigidbody rb;
+
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTorque = 10;
+    private float xRange = 4;
+    private float ySpawnPos = -6;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(RandomForce(), ForceMode.Impulse);
+        rb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+        transform.position = RandomSpawnPosition();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    Vector3 RandomForce() {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    float RandomTorque() {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    Vector3 RandomSpawnPosition() {
+        return new Vector3(Random.Range(-xRange, xRange), -ySpawnPos);
+    }
+
+    private void OnMouseDown() {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        Destroy(gameObject);
     }
 }
